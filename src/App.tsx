e1cbd43b47b4b30371cb886e45e09a74ca34884b
Jessica Churgin <jchurgy@gmail.com>
2025-10-03 18:21:06 -0400

    basic div structure for skills selection

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,17 @@ function App() {
       </div>
       <div className="page-body">
         <div className="sidebar"></div>
-        <SkillsSelection />
+        <div className="project-container">
+          <div className="project-title">
+            {" "}
+            <h1>Project Scope</h1>
+            <p>Tell us what you're looking for and when you want to start.</p>
+          </div>
+
+          <div className="skills-container">
+            <SkillsSelection />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/SkillsSelection/skillsSelection.tsx
+++ src/components/SkillsSelection/skillsSelection.tsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import skillsData from "../../data/skillsData.json";
 import SearchableDropdown from "../SearchableDropdown/searchableDropdown";
+import styled from "styled-components";
 
-export default function SkillsSelectionProps() {
+export const SkillsRow = styled.div`
+  margin-bottom: 1.25rem;
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+export default function SkillsSelection() {
   const [selected, setSelected] = useState<string[]>([]);
   console.log(skillsData);
   console.log(selected, setSelected);
@@ -12,14 +20,14 @@ export default function SkillsSelectionProps() {
   //   };
 
   return (
-    <div>
-      <div className="skills-header"></div>
+    <>
+      <SkillsRow className="skills-header"><p>Agency Skills</p><p>What type of agency are you looking for? Select one or multiple.</p></SkillsRow>
       <div className="skills-dropdown">
         <div className="skills-dropdown__title"></div>
         <SearchableDropdown options={[]} selectedValues={[]} />
       </div>
-      <div className="skills-selected"></div>
-      <div className="skills-suggested"></div>
-    </div>
+      <SkillsRow className="skills-selected"><p aria-label="Selected skills">Selected</p></SkillsRow>
+      <SkillsRow className="skills-suggested"><p aria-label="Suggested skills">Suggested</p></SkillsRow>
+    </>
   );
 }
